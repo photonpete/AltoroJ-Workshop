01c707a7efcf95ba09e4df765e2cbcb82262162d
photonpete <131276155+photonpete@users.noreply.github.com>
2023-04-19 08:52:23 -0700

    Update ServletUtil.java
    adding vuln

--- src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
@@ -344,7 +344,8 @@ public class ServletUtil {
 		    String accountStringList = Account.toBase64List(accounts);
 		    Cookie accountCookie = new Cookie(ServletUtil.ALTORO_COOKIE, accountStringList);
 			session.setAttribute(ServletUtil.SESSION_ATTR_USER, user);
-		    return accountCookie;
+		    	accountCookie.setSecure(false);
+			return accountCookie;
 		}
 		catch(SQLException e){
 			e.printStackTrace();
